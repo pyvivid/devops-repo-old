77f8ba196784071c42603ae92d067272dc5fd460
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2026-07-19 11:50:12 +0530

    Update 01-jenkins-setup.sh
    Updated to clean the older repositories, if any exists.

--- 01-jenkins-setup.sh
+++ 01-jenkins-setup.sh
@@ -28,7 +28,7 @@ apt install -y \
 
 echo ""
 echo "Installing OpenJDK 17..."
-apt install -y openjdk-17-jdk
+apt install -y fontconfig openjdk-21-jdk
 
 echo ""
 echo "Verifying Java installation..."
@@ -38,16 +38,19 @@ echo ""
 echo "Removing any old Jenkins repository..."
 rm -f /etc/apt/sources.list.d/jenkins.list
 rm -f /usr/share/keyrings/jenkins-keyring.asc
+sudo apt clean
 
 echo ""
 echo "Adding Jenkins GPG key..."
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
-| tee /usr/share/keyrings/jenkins-keyring.asc >/dev/null
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
 
 echo ""
-echo "Adding Jenkins repository..."
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
-> /etc/apt/sources.list.d/jenkins.list
+echo "Adding Jenkins Repository"
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 echo ""
 echo "Updating package repository..."
